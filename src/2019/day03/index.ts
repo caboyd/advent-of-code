e1e54d9abcdc83a806e93c9c8ad143c988f1fb91
Chris <boyd111c@gmail.com>
2019-12-07 17:39:33 -0600

    day 3 part two

--- src/2019/day03/index.ts
+++ src/2019/day03/index.ts
@@ -5,5 +5,120 @@ export const day = 3;
 
 export const results: Results = {
     one: 709,
-    two: 0,
+    two: 13836,
+};
+
+export interface Vec2 {
+    x: number;
+    y: number;
+}
+export enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+}
+
+export interface HorizLine {
+    dir: Direction.WEST | Direction.EAST;
+    steps: number;
+    y: number;
+    x_min: number;
+    x_max: number;
+}
+
+export interface VertLine {
+    dir: Direction.NORTH | Direction.SOUTH;
+    steps: number;
+    x: number;
+    y_min: number;
+    y_max: number;
+}
+
+export interface Lines {
+    vert_lines: VertLine[];
+    horiz_lines: HorizLine[];
+}
+
+export const find_line_intersections = (
+    horiz: HorizLine[],
+    vert: VertLine[],
+    distance_function: (h: HorizLine, v: VertLine) => number
+): number => {
+    let shortest_distance = Number.MAX_SAFE_INTEGER;
+
+    for (let i = 0; i < horiz.length; i++) {
+        const l1 = horiz[i];
+        if (l1.x_min === 0) continue;
+        for (let j = 0; j < vert.length; j++) {
+            const l2 = vert[j];
+            if (l2.x === 0) continue;
+            //find intersections
+            if (l1.y < l2.y_max && l1.y > l2.y_min) {
+                if (l2.x < l1.x_max && l2.x > l1.x_min) {
+                    //intersection point
+                    const distance = distance_function(l1, l2);
+
+                    if (distance < shortest_distance) shortest_distance = distance;
+                }
+            }
+        }
+    }
+    return shortest_distance;
+};
+
+export const build_lines = (input: string): Lines[] => {
+    return input.split(/\r?\n/).map(s => {
+        const path_walker = {x: 0, y: 0} as Vec2;
+        let steps = 0;
+        const lines: Lines = {vert_lines: [], horiz_lines: []};
+        s.split(/,/).map(n => {
+            const cardinal = n[0];
+            const len = Number(n.slice(1));
+            switch (cardinal) {
+                case 'L':
+                    lines.horiz_lines.push({
+                        dir: Direction.WEST,
+                        steps: steps,
+                        y: path_walker.y,
+                        x_min: path_walker.x - len,
+                        x_max: path_walker.x,
+                    });
+                    path_walker.x -= len;
+                    break;
+                case 'R':
+                    lines.horiz_lines.push({
+                        dir: Direction.EAST,
+                        steps: steps,
+                        y: path_walker.y,
+                        x_min: path_walker.x,
+                        x_max: path_walker.x + len,
+                    });
+                    path_walker.x += len;
+                    break;
+                case 'U':
+                    lines.vert_lines.push({
+                        dir: Direction.NORTH,
+                        steps: steps,
+                        x: path_walker.x,
+                        y_min: path_walker.y,
+                        y_max: path_walker.y + len,
+                    });
+                    path_walker.y += len;
+                    break;
+                case 'D':
+                    lines.vert_lines.push({
+                        dir: Direction.SOUTH,
+                        steps: steps,
+                        x: path_walker.x,
+                        y_min: path_walker.y - len,
+                        y_max: path_walker.y,
+                    });
+                    path_walker.y -= len;
+                    break;
+            }
+            steps += len;
+        });
+        return lines;
+    });
 };
---
+++ src/2019/day03/part_two.ts
@@ -0,0 +1,30 @@
+import {benchmark, read_input} from '../../lib';
+import {build_lines, day, Direction, find_line_intersections, HorizLine, VertLine, year} from './index';
+
+export const line_distance = (h: HorizLine, v: VertLine): number => {
+    let distance = h.steps + v.steps;
+
+    if (h.dir == Direction.EAST) distance += v.x - h.x_min;
+    else distance += h.x_max - v.x;
+
+    if (v.dir == Direction.NORTH) distance += h.y - v.y_min;
+    else distance += v.y_max - h.y;
+
+    return distance;
+};
+
+export const equation_two = (input: string): number => {
+    const [line1, line2] = build_lines(input);
+
+    return Math.min(
+        find_line_intersections(line1.horiz_lines, line2.vert_lines, line_distance),
+        find_line_intersections(line2.horiz_lines, line1.vert_lines, line_distance)
+    );
+};
+
+if (require.main === module) {
+    (async () => {
+        const input = await read_input(year, day);
+        console.log(`Result: ${benchmark(() => equation_two(input))}`); //13836 ~6.02 ms
+    })();
+}
