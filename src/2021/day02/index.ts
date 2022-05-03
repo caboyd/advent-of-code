68c0245a3d3ae503ab5ac1027b36288068f02e19
Chris <boyd111c@gmail.com>
2022-05-02 18:13:33 -0600

    2021 day 2

---
+++ src/2021/day02/index.ts
@@ -0,0 +1,9 @@
+import {Results} from 'src/lib';
+
+export const year = 2021;
+export const day = 2;
+
+export const results: Results = {
+    one: 1813801,
+    two: 1960569556,
+};
---
+++ src/2021/day02/part_two.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import {benchmark, read_input} from 'src/lib';
+import {day, year} from './index';
+
+export const equation_two = (input: string): number => {
+    let pos = 0;
+    let depth = 0;
+    let aim = 0;
+    const arr = input.split(/\r?\n/);
+    for (const line of arr) {
+        const s = line.split(' ');
+        switch (s[0]) {
+            case 'forward':
+                pos += +s[1];
+                depth += aim * +s[1];
+                break;
+            case 'up':
+                //depth -= +s[1];
+                aim -= +s[1];
+                break;
+            case 'down':
+                // depth += +s[1];
+                aim += +s[1];
+                break;
+        }
+    }
+    return pos * depth;
+};
+
+if (require.main === module) {
+    (async () => {
+        const input = await read_input(year, day);
+        console.log(`Result: ${await benchmark(() => equation_two(input))}`); //1960569556 ~0.834ms
+    })();
+}
