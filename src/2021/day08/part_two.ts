68bae53cebaaca834a8d9e239a3b3eeab7cf92f1
Chris <boyd111c@gmail.com>
2022-05-05 20:19:18 -0600

    fix table indent

--- src/2021/day08/part_two.ts
+++ src/2021/day08/part_two.ts
@@ -81,19 +81,19 @@ const decode = (code: string, segments: string[]): number => {
     const r = parseInt(b, 2);
 
     //determine which number is active using truth table to binary to decimal
-    // 0	1	2	3	4	5	6   <- segment id
-    //64	32	16	8	4	2	1   <- binary value
-    //a	    b	c	d	e	f	g	display num = decimal value
-    //1	    1	1		1	1	1	0 = 119
-    //		    1			1		1 = 18
-    //1		    1	1	1		1	2 = 93
-    //1		    1	1		1	1	3 = 91
-    //	    1	1	1		1		4 = 58
-    //1	    1		1		1	1	5 = 107
-    //1	    1		1	1	1	1	6 = 111
-    //1		    1			1		7 = 82
-    //1	    1	1	1		1	1	9 = 127
-    //1	    1	1	1	1	1	1	8 = 123
+    //0		1	2	3	4	5	6	<- segment id
+    //64	32	16	8	4	2	1	<- binary value
+    //a		b	c	d	e	f	g	display num = decimal value
+    //1		1	1		1	1	1	0 = 119
+    //			1			1		1 = 18
+    //1			1	1	1		1	2 = 93
+    //1			1	1		1	1	3 = 91
+    //		1	1	1		1		4 = 58
+    //1		1		1		1	1	5 = 107
+    //1		1		1	1	1	1	6 = 111
+    //1			1			1		7 = 82
+    //1		1	1	1		1	1	9 = 127
+    //1		1	1	1	1	1	1	8 = 123
     if (r == 119) return 0;
     else if (r == 18) return 1;
     else if (r == 93) return 2;
